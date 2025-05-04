f20982437a87194e6a70fac4fda02648dd440dea
RahmDridiCode <dridirahma951@gmail.com>
2025-05-04 23:57:08 +0200

    implement edit profile

--- src/main/java/com/fstg/mediatech/controllers/AuthController.java
+++ src/main/java/com/fstg/mediatech/controllers/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.nio.file.Files;
